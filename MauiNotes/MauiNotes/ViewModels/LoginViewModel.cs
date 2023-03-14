1d01dd6a4f6eb21fb50592469b5995227ebbaf80
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2023-03-13 18:39:57 -0700

    start of 02_02

---
+++ MauiNotes/MauiNotes/ViewModels/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MauiNotes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiNotes.ViewModels
+{
+    public class LoginViewModel: BaseViewModel
+    {
+        private ILoginService _LoginService;
+        private IGlobalNavigation _GlobalNavigation;
+
+        public LoginViewModel(ILoginService loginService,
+            IGlobalNavigation globalNavigation)
+        {
+            _LoginService = loginService;
+            _GlobalNavigation = globalNavigation;
+        }
+
+        private Command _LoginCommand;
+
+        public Command Login
+        {
+            get { return _LoginCommand?? new Command(async () => await ExecuteLogin()); }
+        }
+
+        private async Task ExecuteLogin()
+        {
+            _LoginService.Login("", "");
+
+            await _GlobalNavigation.XamlNavigation.PopModalAsync();
+        }
+    }
+}
---
+++ MauiNotes/MauiNotes/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MauiNotes.Interfaces;
+using MauiNotes.Services;
+using MauiNotes.ViewModels;
+
+namespace MauiNotes.Views;
+
+public partial class LoginPage : ContentPage
+{
+	private ILoginService _LoginService;
+
+	public LoginPage(ILoginService loginService, LoginViewModel loginViewModel)
+	{
+		InitializeComponent();
+
+        _LoginService = loginService;
+
+		this.BindingContext = loginViewModel;
+	}
+
+}
+
