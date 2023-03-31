60397b0b12743dad17407f4be1ef50c34489c687
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2023-03-30 22:16:50 -0700

    start of 03_04

--- MauiNotes/Notes.Library/wwwroot/Notes.Library.lib.module.js
+++ MauiNotes/Notes.Library/wwwroot/Notes.Library.lib.module.js
@@ -5,4 +5,8 @@
     sharedScripts.setAttribute('src', '_content/Notes.Library/js/sharedInjectedMethods.js');
     document.body.appendChild(sharedScripts);
 
+    var sharedCss = document.createElement('link');
+    sharedCss.setAttribute('rel', 'stylesheet');
+    sharedCss.setAttribute('href', '_content/Notes.Library/css/library.css');
+    document.head.appendChild(sharedCss);
 }
