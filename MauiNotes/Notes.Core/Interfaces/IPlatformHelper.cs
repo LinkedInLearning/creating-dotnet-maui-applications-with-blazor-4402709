7a194f6b92c81516927fe0804cce95a28a9202d6
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2023-04-01 21:25:25 -0700

    Start of 03_04

---
+++ MauiNotes/Notes.Core/Interfaces/IPlatformHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Core.Interfaces
+{
+    public enum PlatformType
+    {
+        Unsupported = 0,
+        WasmWeb = 1,
+        iOS = 2,
+        Android = 3,
+    }
+    public interface IPlatformHelper
+    {
+        PlatformType PlatformType { get; }
+    }
+}
