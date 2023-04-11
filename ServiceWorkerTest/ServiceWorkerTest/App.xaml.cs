62a771b4d8975146fd314830e03541817438bb06
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2023-04-10 20:44:57 -0700

    Start of 04_02

---
+++ ServiceWorkerTest/ServiceWorkerTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceWorkerTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ ServiceWorkerTest/ServiceWorkerTest/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace ServiceWorkerTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        blazorWebView.UrlLoading += BlazorWebView_UrlLoading;
+
+    }
+
+    private void BlazorWebView_UrlLoading(object sender, Microsoft.AspNetCore.Components.WebView.UrlLoadingEventArgs e)
+    {
+        e.UrlLoadingStrategy = Microsoft.AspNetCore.Components.WebView.UrlLoadingStrategy.OpenInWebView;
+    }
+}
