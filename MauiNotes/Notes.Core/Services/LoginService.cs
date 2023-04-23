99bd72d7027fba1537cf047b688f95e8ebd39580
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2023-04-22 20:28:16 -0700

    End of 04_04

--- MauiNotes/Notes.Core/Services/LoginService.cs
+++ MauiNotes/Notes.Core/Services/LoginService.cs
@@ -9,37 +9,47 @@ namespace Notes.Core.Services
 {
     public class LoginService : ILoginService
     {
-        private bool _isAuthenticated = false;
+        private static string _token = "";
+        private readonly IPlatformHelper _platformHelper;
+        private readonly IKeyValueStorageService _localStore;
 
-        public LoginService()
+        private readonly string AUDIENCE = "https://MauiBlazor.org";
+        private readonly string CLIENT_ID = "btxhnjTeMdQfJbWvUVmZzvfGhctICq5r";
+        private readonly string AUTH_URI = "https://bowman74.auth0.com/oauth/token";
+
+        private readonly string OFFLINE_EXPIRATION = "OfflineExpiration";
+
+        public LoginService(IKeyValueStorageService localStore,
+                    IPlatformHelper platformHelper)
         {
+            _localStore = localStore;
+            _platformHelper = platformHelper;
         }
 
         public Task<bool> IsAuthenticated()
         {
             var tcs = new TaskCompletionSource<bool>();
-            tcs.SetResult(_isAuthenticated);
+            tcs.SetResult(true);
             return tcs.Task;
         }
 
         public Task<bool> Login(string username, string password)
         {
             var tcs = new TaskCompletionSource<bool>();
-            _isAuthenticated = true;
-            tcs.SetResult(_isAuthenticated);
+            tcs.SetResult(true);
 
             return tcs.Task;
         }
 
-        private Task Logout()
+        private async Task Logout()
         {
-            _isAuthenticated = false;
-            return Task.CompletedTask;
+            _token = string.Empty;
+            await _localStore.RemoveValue(OFFLINE_EXPIRATION);
         }
 
         public string CurrentToken()
         {
-            return string.Empty;
+            return _token;
         }
     }
 }
